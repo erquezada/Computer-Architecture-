59b3d4ae98b787bd0e583e95bbb3765cab854360
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2024-12-21 10:15:35 -0700

    Ascii Art Project

---
+++ Ascii-Art/draw_shapes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "draw.h"
+
+/* Prints a size x size square whose left col is at startCol */
+void print_square(int leftCol, int size)
+{
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+// Prints a triangle of specified height whose left edge is at col leftCol.
+void print_triangle(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+}
+// prints an arrow using ascii characters
+void print_arrow(int leftCol, int size) {
+  // call the print_triangle function. This will be the pointy part of the arrow.
+  print_triangle(5,5);
+  // call the print_square function. This will be base part of the arrow. 
+  print_square(8,5);
+}
